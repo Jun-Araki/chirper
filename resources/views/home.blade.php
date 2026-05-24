85bf8bdc62bde3e66c46c481d57ff44ee9b7594e
Jun Araki <jun7772006@gmail.com>
2026-05-24 18:13:26 +0900

    What is MVC?

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,16 @@
         Welcome
     </x-slot:title>
     <div class="max-w-2xl mx-auto">
+        @foreach ($chirps as $chirp)
         <div class="card bg-base-100 shadow mt-8">
             <div class="card-body">
                 <div>
-                    <h1 class="text-3xl font-bold">Welcome to Chirper!</h1>
-                    <p>This is your brand new Laravel app. Time to make it sing (or chirp)!</p>
-                    <p class="mt-2 text-sm text-gray-600">Now this is live on the internet! 🎉</p>
+                    <h1 class="font-semibold">{{ $chirp['author'] }}</h1>
+                    <p class="mt-1">{{ $chirp['message'] }}</p>
+                    <p class="text-ms text-gray-400 mt-2">{{ $chirp['time'] }}</p>
                 </div>
             </div>
         </div>
+        @endforeach
     </div>
 </x-layout>
